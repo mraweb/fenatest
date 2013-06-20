b2ea6859df19bdde9f5299db7912cb61cb9305f6
Marcelo Almeida <marcelo@mraweb.com.br>
2013-06-20 17:32:15 -0300

    Inicio do HTML

---
+++ index.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8" />
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+<title>Brasil FENATEST</title>
+<!--<link rel="stylesheet" type="text/css" href="css/style.css">-->
+<!--[if ie]>
+	<script type="text/javascript" src="js/html5-ie.js"></script>
+<![endif]-->
+</head>
+<body itemscope itemtype="http://schema.org/WebPage">
+
+<div class="bg-home">
+    <header role="banner">
+        <h1><a href="/" title="Brasil FENATEST" class="hidetxt">Brasil FENATEST</a></h1>
+
+        <div class="descricao">
+            <h2>Federação Nacional dos Técnicos de Segurança do Trabalho</h2>
+
+            <p></p>
+        </div>
+
+        <div class="midias">
+            <ul>
+                <li><a href="#" title="Facebook" class="bt-face">Facebook</a></li>
+                <li><a href="#" title="Twitter" class="bt-twitter">Twitter</a></li>
+                <li><a href="#" title="Youtube" class="bt-youtube">Youtube</a></li>
+            </ul>
+
+            <img src="" alt="" width="" height="" />
+        </div>
+        
+        <div class="menu">
+            <nav role="navigation">
+                <ul>
+                    <li><a href="/" title="Home" class="bt-home">Home</a></li>
+                    <li><a href="diretoria.php" title="Diretoria" class="bt-dire">Diretoria</a></li>
+                    <li><a href="noticias.php" title="Notícias" class="bt-noti">Notícias</a></li>
+                    <li><a href="galeria.php" title="Galeria" class="bt-gale">Galeria</a></li>
+                    <li><a href="links.php" title="Links" class="bt-link">Links</a></li>
+                    <li><a href="legislacao.php" title="Legislação" class="bt-legi">Legislação</a></li>
+                    <li><a href="sindicatos.php" title="Sindicatos" class="bt-sind">Sindicatos</a></li>
+                    <li><a href="guias.php" title="Guias" class="bt-guia">Guias</a></li>
+                    <li><a href="fale-conosco.php" title="Fale Conosco" class="bt-fale">Fale Conosco</a></li>
+                </ul>
+            </nav>
+
+            <div class="busca">
+                <label for="busca">Busca</label>
+                <input type="text" name="busca" id="busca" />
+                <input type="image" src="img/bt-buscar.jpg" alt="Buscar" name="Buscar" class="bt-buscar" />
+            </div>
+        </div>
+    </header><!-- FINAL HEADER -->
+    
+    <section id="content" role="main">
+        <div class="cont-left">
+            <div id="slide-home">
+                <div class="slides_container">
+                    <section class="slides">
+                        <img src="" alt="" width="" height="" />
+
+                        <div class="texto">
+                            <h3>Lançada em SP a Campanha pela revisão do FGTS</h3>
+
+                            <p>loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsumloren ipsum loren ipsumloren loren ipsum loren ipsum loren ipsum</p>
+                        </div>
+                    </section>
+                </div>
+            </div><!-- FINAL SLIDE HOME -->
+
+            <div>
+
+            </div>
+
+
+
+
+
+
+
+
+        </div><!-- FINAL CONT-LEFT -->
+
+
+
+
+
+
+
+
+
+
+
+
+    </section><!-- FINAL CONTENT -->  
+</div>
+
+<div class="bg-rodape">
+    <footer role="contentinfo">
+
+    </footer>
+</div>
+<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+<script type="text/javascript" src="js/scripts.js"></script>
+</body>
+</html>
