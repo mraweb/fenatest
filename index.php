33736342e56bd57f8dcef8d57c01326ee85ccee1
Marcelo Almeida <marcelo@mraweb.com.br>
2013-06-21 00:17:07 -0300

    Estrutura HTML Finalizado

--- index.php
+++ index.php
@@ -64,45 +64,206 @@
                         <div class="texto">
                             <h3>Lançada em SP a Campanha pela revisão do FGTS</h3>
 
-                            <p>loren ipsum loren ipsum loren ipsum loren ipsum loren ipsum loren ipsumloren ipsum loren ipsumloren loren ipsum loren ipsum loren ipsum</p>
+                            <p>Diretores e assessores do nosso Sindicato participaram nesta sexta-feira, 7 de junho, do lançamento da campanha pela recuperação das perdas do FGTS (Fundo de Garantia do Tempo.</p>
                         </div>
                     </section>
                 </div>
             </div><!-- FINAL SLIDE HOME -->
 
-            <div>
+            <div class="conselho">
+                <h3>Conselho de Classe dos Técnicos de Segurança do Trabalho</h3>
 
+                <a href="#" title="Confestest">Confestest<br /><span>Clique aqui</span></a>
             </div>
 
+            <aside class="menu-left">
+                <nav role="navigation">
+                    <h3>Menu</h3>
 
+                    <ul>
+                        <li><a href="" title="Calendário">Calendário</a></li>
+                        <li><a href="" title="Indicadores de SST">Indicadores de SST</a></li>
+                        <li><a href="" title="Convenções Coletivas">Convenções Coletivas</a></li>
+                        <li><a href="" title="Convênios">Convênios</a></li>
+                        <li><a href="" title="Parceiros">Parceiros</a></li>
+                        <li><a href="" title="Projeto Político">Projeto Político</a></li>
+                        <li><a href="" title="Plano de Trabalho">Plano de Trabalho</a></li>
+                        <li><a href="" title="NRs em mudanças">NRs em mudanças</a></li>
+                        <li><a href="" title="Projetos de Lei">Projetos de Lei</a></li>
+                        <li><a href="" title="Instituições">Instituições</a></li>
+                        <li><a href="" title="Comissões Tripartite">Comissões Tripartite</a></li>
+                    </ul>
 
+                    <h3>Comissões de trabalho</h3>
 
+                    <ul>
+                        <li><a href="" title="Administração">Administração</a></li>
+                        <li><a href="" title="Gestão Financeira">Gestão Financeira</a></li>
+                        <li><a href="" title="Rel. Institucionais">Rel. Institucionais</a></li>
+                        <li><a href="" title="Formação e Registros">Formação e Registros</a></li>
+                        <li><a href="" title="Eventos">Eventos</a></li>
+                        <li><a href="" title="Legislação">Legislação</a></li>
+                        <li><a href="" title="Exercício da Profissão">Exercício da Profissão</a></li>
+                        <li><a href="" title="Estatísticas">Estatísticas</a></li>
+                        <li><a href="" title="Convenções">Convenções</a></li>
+                        <li><a href="" title="Comunicação">Comunicação</a></li>
+                        <li><a href="" title="Estatuto">Estatuto</a></li>
+                    </ul>
+                </nav>
+            </aside><!-- FINAL MENU LEFT -->
 
+            <section class="cont-central">
+                <h3 class="h-fena">Boletim FENATEST</h3>
+                
+                <article class="artigo">
+                    <img src="" alt="Turista alemão baleado na Rocinha" width="" height="" />
+                    
+                    <h4><a href="#" title="Turista alemão baleado na Rocinha vai conhecer o Cristo antes de viajar:" class="green">Turista alemão baleado na Rocinha vai conhecer o Cristo antes de viajar:</a></h4>
 
+                    <p>O turista alemão Daniel Benjamin Frank, de 25 anos, que foi baleado por traficantes durante uma visita à Favela da Rocinha, na Zona Sul do Rio, vai conhecer o Cristo Redentor nesta terça-feira (11). O jovem deve...  <span class="green">Veja mais</span></p>
+                </article>
 
+                <section class="boletins">
+                    <h4>Veja mais de nosso Boletim:</h4>
 
+                    <ul class="lista-01">
+                        <li><a href="#" title="">Brasil é apontado como país mais protecionista dentro do G-20</a></li>
+                        <li><a href="#" title="">Governo estuda facilitar mais a entrada de dólares no mercado</a></li>
+                        <li><a href="#" title="">Busque artista, música ou temaBuscar</a></li>
+                        <li><a href="#" title="">Wanessa está agitando as pistas; ouça pop nacional</a></li>
+                        <li><a href="#" title="">Programa tem Beirut, B.B. King e Ella Fitzgerald (foto)</a></li>
+                    </ul>
+
+                    <h4>Boletim anteriores:</h4>
+
+                    <ul class="lista-02">
+                        <li><a href="" title="">Brasil é apontado como o país...</a></li>
+                        <li><a href="" title="">Governo estuda facilitar mais a...</a></li>
+                        <li><a href="" title="">Busque artista, música ou tema...</a></li>
+                        <li><a href="" title="">Brasil é apontado como o país...</a></li>
+                        <li><a href="" title="">Governo estuda facilitar mais a...</a></li>
+                        <li><a href="" title="">Busque artista, música ou tema...</a></li>
+                    </ul>
+                </section><!-- FINAL BOLETINS -->
+            </section><!-- FINAL CONT CENTRAL -->
         </div><!-- FINAL CONT-LEFT -->
 
+        <aside class="menu-right">
+            <section class="video">
+                <img src="" alt="" width="" height="" />
 
+                <h4>Vídeo</h4>
 
+                <p><a href="" title="">Jose Augusto parabeniza Armando Henrique. <span class="green">Veja</span></a></p>
+            </section><!-- FINAL VIDEO -->
 
+            <section class="outros-videos">
+                <h4>Veja outros vídeos</h4>
 
+                <ul>
+                    <li><a href="" title="">São Paulo registra um arrastão...</a></li>
+                    <li><a href="" title="">São Paulo registra um arrastão...</a></li>
+                    <li><a href="" title="">São Paulo registra um arrastão...</a></li>
+                    <li><a href="" title="">São Paulo registra um arrastão...</a></li>
+                </ul>
 
+                <p class="bt-mais"><a href="" title="Veja Mais">Veja Mais</a></p>
+            </section><!-- FINAL OUTROS VIDEOS -->
 
+            <section class="parceiros">
+                <h4>Parceiros e Patrocinadores</h4>
 
+                <ul>
+                    <li><a href="" title="Publicidade"><img src="http://placehold.it/209x71&text=Publicidade" alt="Publicidade" width="209" height="71" /></a></li>
 
+                    <li><a href="" title="Publicidade"><img src="http://placehold.it/209x71&text=Publicidade" alt="Publicidade" width="209" height="71" /></a></li>
 
+                    <li><a href="" title="Publicidade"><img src="http://placehold.it/209x71&text=Publicidade" alt="Publicidade" width="209" height="71" /></a></li>
 
+                    <li><a href="" title="Publicidade"><img src="http://placehold.it/209x71&text=Publicidade" alt="Publicidade" width="209" height="71" /></a></li>
 
+                    <li><a href="" title="Publicidade"><img src="http://placehold.it/209x71&text=Publicidade" alt="Publicidade" width="209" height="71" /></a></li>
+                </ul>
+            </section><!-- FINAL PARCEIROS -->
+
+            <section class="estatistica">
+                <h4>Estatísticas</h4>
+
+                <ul>
+                    <li>Usuários Online: <span class="green">17</span></li>
+                    <li>Essa semana: <span class="green">5325</span></li>
+                    <li>Total de visítas: <span class="green">955321</span></li>
+                </ul>
+
+                <p><a href="" title="Publicidade"><img src="http://placehold.it/209x110&text=Publicidade" alt="Publicidade" width="209" height="110" /></a></p>
+            </section><!-- FINAL ESTATISTICA -->
+        </aside><!-- FINAL MENU RIGHT -->
     </section><!-- FINAL CONTENT -->  
 </div>
 
 <div class="bg-rodape">
     <footer role="contentinfo">
+        <nav class="boas-praticas" role="navigation">
+            <h3>Menu</h3>
 
+            <ul>
+                <li><a href="" title="Pessoa Física">Pessoa Física</a></li>
+                <li><a href="" title="Pessoa Jurídica">Pessoa Jurídica</a></li>
+            </ul>
+        </nav><!-- FINAL BOAS PRATICAS -->
+
+        <section class="banner-footer">
+            <h4>Publicidade</h4>
+
+            <p><a href="" title="Publicidade"><img src="http://placehold.it/728x90&text=Publicidade" alt="Publicidade" width="728" height="90" /></a></p>
+        </section><!-- FINAL BANNER FOOTER -->
+
+        <div class="bottom">
+            <address>
+                Brasil FENATEST
+                Sede: Rua 24 de Maio, 104 – 1º Andar – 01041-000 – Centro - São Paulo - SP<br />
+                fenatest@fenatest.org.br – www.fenatest.org.br<br />
+                Fone: (11) 3331-2700 Fax: (11)3222-9399
+            </address>
+        </div><!-- FINAL BOTTOM -->
+
+        <div class="midias-footer">
+            <h5>Siga nas Redes Sociais</h5>
+
+            <ul>
+                <li><a href="#" title="Facebook" class="bt-face">Facebook</a></li>
+                <li><a href="#" title="Twitter" class="bt-twitter">Twitter</a></li>
+                <li><a href="#" title="Youtube" class="bt-youtube">Youtube</a></li>
+            </ul>
+        </div><!-- FINAL MIDIAS FOOTER -->
+
+        <p class="estados">Estados Filiados: AL-AP-AM-BA-CE-DF-ES-GO-MA-MT-MS-MG-PA-PB-PR-PI-RJ-RN-RS-RO-RR-SC-SP-SE-AC-TO</p>
+
+        <a href="#" title="Voltar ao Topo" class="bt-voltar hidetxt">Voltar ao Topo</a>
     </footer>
 </div>
+
 <script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
+
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
